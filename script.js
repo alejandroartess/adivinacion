72afbc9b4521e92d5494a7cd66806c467a5f03c6
ALUMNADO\alarso <alarso@hotmail.com>
2017-10-06 11:39:08 +0200

    adivinacion1

---
+++ script.js
@@ -0,0 +1,8 @@
+var letracorrecta="a"
+var respuesta=prompt("Escribe una letra y prueba suerte!")
+if (letracorrecta==respuesta) {
+  alert("Has acertado,felicidadees!")
+}
+if (letracorrecta!=respuesta) {
+  alert("No has acertado,intentalo de nuevo!")
+}
